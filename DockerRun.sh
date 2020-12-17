2b1f576f3ca4bce7cca40282cea1b07e9b19fe34
Chathura Colombage <chathura.colombage@quantcast.com>
2020-12-17 12:31:35 +0000

    add incremental flag to faster changes locally

--- DockerRun.sh
+++ DockerRun.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env sh
 
-docker run --rm --volume=$(pwd):/srv/jekyll -it -p 4000:4000 jekyll/jekyll jekyll s --config _config.yml
+docker run --rm --volume=$(pwd):/srv/jekyll -it -p 4000:4000 jekyll/jekyll jekyll s --config _config.yml --incremental
